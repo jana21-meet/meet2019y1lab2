1ece4a0ebe69adc3dc20207e30a2428dbd31c59c
jana21-meet <jana21@meet.mit.edu>
2019-07-23 05:08:43 -0400

    Stopping here

---
+++ AgeCompareCalculator.py
@@ -0,0 +1,7 @@
+person_one=input("what is the age of the first person?")
+person_2=input("what is the age of the second person?")
+person_one=float(person_one)
+person_2=float(person_2)
+defer=person_one - person_2
+defer=abs(defer)
+print(defer)
